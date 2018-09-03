7578e5441935cde5ea70e5d231a1e3fd98df4ad0
Michael <michael.egwali@andela.com>
2018-09-03 10:49:08 +0100

    create basic server

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => res.status(200).send({ message: 'Bored were we? Now lost' }));
+
+module.exports = app;
