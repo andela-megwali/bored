e62c943bd8135fcfe38798eb3ad8140be5b3ce77
Michael <michael.egwali@andela.com>
2018-09-17 08:20:17 +0100

    Update package.json scripts

--- server/config/config.js
+++ server/config/config.js
@@ -1,26 +1,28 @@
+require('dotenv').config();
+
 module.exports = {
   'development': {
     'username': process.env.DEV_DB_USERNAME,
     'password': process.env.DEV_DB_PASSWORD,
-    'database': 'd3-practice1-todo',
+    'database': process.env.DEV_DB,
     'host': process.env.DEV_HOST,
-    'dialect': 'postgres',
+    'dialect': process.env.DEV_DIALECT,
     'secret': process.env.DEV_SECRET,
   },
   'test': {
-    'username': 'enigma',
-    'password': null,
-    'database': 'database_test',
-    'host': '127.0.0.1',
-    'dialect': 'postgres',
-    'secret': 'abcdefg',
+    'username': process.env.TEST_DB_USERNAME,
+    'password': process.env.TEST_DB_PASSWORD,
+    'database': process.env.TEST_DB,
+    'host': process.env.TEST_HOST,
+    'dialect': process.env.TEST_DIALECT,
+    'secret': process.env.TEST_SECRET,
   },
   'production': {
-    'username': process.env.DEV_DB_USERNAME,
-    'password': process.env.DEV_DB_PASSWORD,
-    'database': process.env.DEV_DB,
-    'host': process.env.DEV_HOST,
-    'dialect': 'postgres',
-    'secret': process.env.DEV_SECRET,
+    'username': process.env.PROD_DB_USERNAME,
+    'password': process.env.PROD_DB_PASSWORD,
+    'database': process.env.PROD_DB,
+    'host': process.env.PROD_HOST,
+    'dialect': process.env.PROD_DIALECT,
+    'secret': process.env.PROD_SECRET,
   },
 };
